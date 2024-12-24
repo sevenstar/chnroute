0522a83b550959d0e56742db432acd72f1d874de
sevenstar <277626504@qq.com>
2024-12-24 16:08:28 +0800

    1

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -16,16 +16,18 @@ wget --no-check-certificate -c -O cernet.txt https://ispip.clang.cn/cernet_cidr.
 wget --no-check-certificate -c -O gwbn.txt https://ispip.clang.cn/gwbn_cidr.txt
 #其他
 wget --no-check-certificate -c -O other.txt https://ispip.clang.cn/othernet_cidr.txt
+#CN
+wget --no-check-certificate -c -O all_cn_cidr.txt https://ispip.clang.cn/all_cn_cidr.txt
 
 {
 echo "/ip route rule"
 
 for net in $(cat ct.txt) ; do
-  echo "add dst-address=$net action=lookup table=CT"
+  echo "add dst-address=$net action=lookup table=CU"
 done
 
 for net in $(cat cu.txt) ; do
-  echo "add dst-address=$net action=lookup table=CT"
+  echo "add dst-address=$net action=lookup table=CU"
 done
 
 for net in $(cat cm.txt) ; do
@@ -33,62 +35,28 @@ for net in $(cat cm.txt) ; do
 done
 
 for net in $(cat crtc.txt) ; do
-  echo "add dst-address=$net action=lookup table=CMCC"
+  echo "add dst-address=$net action=lookup table=CU"
 done
 
 for net in $(cat cernet.txt) ; do
-  echo "add dst-address=$net action=lookup table=CT"
+  echo "add dst-address=$net action=lookup table=CU"
 done
 
 for net in $(cat gwbn.txt) ; do
-  echo "add dst-address=$net action=lookup table=CT"
+  echo "add dst-address=$net action=lookup table=CU"
 done
 
 for net in $(cat other.txt) ; do
-  echo "add dst-address=$net action=lookup table=CT"
+  echo "add dst-address=$net action=lookup table=CU"
 done
-} > ../ros-pbr-CT-CMCC.rsc
+} > ../ros-pbr-CU-CMCC.rsc
 
 
 {
 echo "/ip firewall address-list"
 
 for net in $(cat ct.txt) ; do
-  echo "add list=dpbr-CT address=$net"
-done
-
-for net in $(cat cu.txt) ; do
-  echo "add list=dpbr-CT address=$net"
-done
-
-for net in $(cat cm.txt) ; do
-  echo "add list=dpbr-CMCC address=$net"
-done
-
-for net in $(cat crtc.txt) ; do
-  echo "add list=dpbr-CMCC address=$net"
-done
-
-for net in $(cat cernet.txt) ; do
-  echo "add list=dpbr-CT address=$net"
-done
-
-for net in $(cat gwbn.txt) ; do
-  echo "add list=dpbr-CT address=$net"
-done
-
-for net in $(cat other.txt) ; do
-  echo "add list=dpbr-CT address=$net"
-done
-} > ../ros-dpbr-CT-CMCC.rsc
-
-
-######电信联通
-{
-echo "/ip firewall address-list"
-
-for net in $(cat ct.txt) ; do
-  echo "add list=dpbr-CT address=$net"
+  echo "add list=dpbr-CU address=$net"
 done
 
 for net in $(cat cu.txt) ; do
@@ -96,26 +64,69 @@ for net in $(cat cu.txt) ; do
 done
 
 for net in $(cat cm.txt) ; do
-  echo "add list=dpbr-CT address=$net"
+  echo "add list=dpbr-CMCC address=$net"
 done
 
 for net in $(cat crtc.txt) ; do
-  echo "add list=dpbr-CT address=$net"
+  echo "add list=dpbr-CU address=$net"
 done
 
 for net in $(cat cernet.txt) ; do
-  echo "add list=dpbr-CT address=$net"
+  echo "add list=dpbr-CU address=$net"
 done
 
 for net in $(cat gwbn.txt) ; do
-  echo "add list=dpbr-CT address=$net"
+  echo "add list=dpbr-CU address=$net"
 done
 
 for net in $(cat other.txt) ; do
-  echo "add list=dpbr-CT address=$net"
+  echo "add list=dpbr-CU address=$net"
 done
-} > ../ros-dpbr-CT-CU.rsc
+} > ../ros-dpbr-CU-CMCC.rsc
 
+
+######联通移动
+{
+echo "/ip firewall address-list"
+
+for net in $(cat ct.txt) ; do
+  echo "add list=dpbr-CU address=$net"
+done
+
+for net in $(cat cu.txt) ; do
+  echo "add list=dpbr-CU address=$net"
+done
+
+for net in $(cat cm.txt) ; do
+  echo "add list=dpbr-CM address=$net"
+done
+
+for net in $(cat crtc.txt) ; do
+  echo "add list=dpbr-CU address=$net"
+done
+
+for net in $(cat cernet.txt) ; do
+  echo "add list=dpbr-CU address=$net"
+done
+
+for net in $(cat gwbn.txt) ; do
+  echo "add list=dpbr-CU address=$net"
+done
+
+for net in $(cat other.txt) ; do
+  echo "add list=dpbr-CU address=$net"
+done
+} > ../ros-dpbr-CU-CM.rsc
+
+######all_cn_cidr
+{
+echo "/ip firewall address-list"
+
+for net in $(cat all_cn_cidr.txt) ; do
+  echo "add list=dpbr-CN address=$net"
+done
+
+} > ../ros-dpbr-CN.rsc
 
 cd ..
 rm -rf ./pbr
